ba55d7bc1e9557273d8bdfa4f402f6538f4df11d
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-04-22 19:53:22 -0300

    create method to cancel plan, getNumberInstallments

--- solution-hibernate-mysql/HibernateTestJPA/src/PlanController.java
+++ solution-hibernate-mysql/HibernateTestJPA/src/PlanController.java
@@ -24,18 +24,12 @@ public class PlanController {
 	 }
 	 
 	public void remover(Plans _plan) {
-		//em.persist(_user);
         em.getTransaction().begin();
-        //em.persist(_user);
-		//Query q = em.createQuery("DELETE FROM Users WHERE id = "+_user.getUserid());
-		//q.executeUpdate();
+
     	System.out.println(_plan.getPlanId());
         Plans p1 = em.find(Plans.class,_plan.getPlanId());
-        System.out.println(p1);
-		//Users uObj = em.find(Users.class, 101);
 		em.remove(p1);
         em.getTransaction().commit();
-		//factory.close();
 		
 	}
 	
@@ -55,7 +49,22 @@ public class PlanController {
 		em.getTransaction().commit();
 	}
 	
+	public int getParcelas(Users _user) {
+        Plans planUser = em.find(Plans.class, _user.getUserid());
+        return planUser.getRemainingInstallments();
+ 
+	}
 	
+	 public void cancelPlan(Users _user) {
+	      Plans planUser = em.find(Plans.class, _user.getUserid());
+	      UsersController usersControllerToGetBalance = new UsersController();
+	      float valueBalance = usersControllerToGetBalance.getBalance(_user);
+	      
+	      System.out.println("Sua conta vai ser cancelada");
+	      float yourValueToWithdrawIs = (valueBalance/100)*20;
+	      System.out.println(yourValueToWithdrawIs);
+	     
+	  }
 	
 }
 
