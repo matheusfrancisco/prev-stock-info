0e6667f6b724702100a291fa6205e4cb696134d5
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-04-28 19:21:12 -0300

    Add models user,plan

---
+++ solution-1/backend/models/Plans.js
@@ -0,0 +1,89 @@
+
+class Plans{
+    constructor(){
+        this._id;
+        this._remainingInstallments;
+        this._numberOfInstallments;
+        this._monthsOfGracePeriod;
+        this._valuePlan;
+        this._idUser;
+        this._paymentAmount;
+        this._payday;
+        this._startDateUser;
+
+    }
+
+    setIdPlan(id){
+        this._id = id;
+    }
+
+    getIdPlan(){
+        return this._id;
+    }
+
+    setRemainingInstallments(remaingInstallments){
+        this._remainingInstallments = remaingInstallments;
+    }
+
+    getRemainingInstallments(){
+        return this._remainingInstallments;
+    }
+
+    setMonthsOfGracePeriod(monthsOfGracePeriod){
+        this._monthsOfGracePeriod = monthsOfGracePeriod;
+    }
+
+    getMonthsOfGracePeriod(){
+        return this._monthsOfGracePeriod;
+    }
+
+
+    setNumberOfInstallments(numberOfInstallments){
+        this._numberOfInstallments = numberOfInstallments;
+    }
+
+    getNumberOfInstallments(){
+        return this._numberOfInstallments;
+    }
+
+    setValuePlan(valuePlan){
+        this._valuePlan;
+    }
+
+    getValuePlan(){
+        return this._valuePlan;
+    }
+
+    setUserId(idUser){
+        this._idUser = idUser;
+    }
+
+    getUserId(){
+        return this._idUser;
+    }
+
+    setPaymentAmount(paymentAmount){
+        this._paymentAmount = paymentAmount;
+    }
+
+    getPaymentAmount(){
+        return this._paymentAmount;
+    }
+
+    setPayday(payday){
+        this._payday = payday;
+    }
+
+    getPayday(){
+        return this._payday;
+    }
+
+    setStartDateUser(startDateUser){
+        this._startDateUser = startDateUser;
+    }
+
+    getStartDateUser(){
+        return this._startDateUser;
+    }
+
+}
