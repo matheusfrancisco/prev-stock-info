1569eeedf39a9c7c8af6c85d9a5894a357a5130d
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-04-28 19:20:03 -0300

    class UserController

---
+++ solution-1/backend/controller/UserController.js
@@ -0,0 +1,86 @@
+const User = require('../models/User.js')
+const db = require('../src/server.js');
+
+
+class UserController{
+
+    insertUser(req, res){
+
+        //let querySql = `INSERT INTO Users (email, cpf, id , senha, nome, endereco, balance) VALUES (${req.body['email']}, ${req.body['cpf']}, ${req.body['cpf']}, ${req.body['id']},${req.body['senha']}, ${req.body['nome']},${req.body['endereco']}, ${0})`;
+        var query = db.query('INSERT INTO Users SET ?', req.body, (error, results, fields) => {
+    	    if (error) throw error;
+            res.send('User insert');
+    	});
+    }
+
+
+    async findUser(cpf , user){
+
+        let columns = ['id', 'nome', 'cpf', 'email', 'endereco', 'balance'];
+        return new Promise((resolve, reject) =>{
+
+        db.query('SELECT ?? FROM ?? Where cpf = ?', [columns, 'Users', cpf], (err, results, fields)=>{
+
+            if(err) throw err;
+            let resultString = JSON.stringify(results);
+
+            let userJson = JSON.parse(resultString);
+
+            console.log(userJson[0])
+            user.setUserName(userJson[0].nome);
+            user.setCpf(userJson[0].cpf);
+            user.setEndereco(userJson[0].endereco);
+            user.setEmail(userJson[0].email);
+            user.setUserId(userJson[0].id);
+
+            resolve(user);
+           });
+        });
+
+    }
+
+    async findUserById(id , user){
+
+        let columns = ['id', 'nome', 'cpf', 'email', 'endereco', 'balance'];
+        return new Promise((resolve, reject) =>{
+
+        db.query('SELECT ?? FROM ?? Where id = ?', [columns, 'Users', id], (err, results, fields)=>{
+
+            if(err) throw err;
+            let resultString = JSON.stringify(results);
+
+            let userJson = JSON.parse(resultString);
+
+            console.log(userJson[0])
+            user.setUserName(userJson[0].nome);
+            user.setCpf(userJson[0].cpf);
+            user.setEndereco(userJson[0].endereco);
+            user.setEmail(userJson[0].email);
+            user.setUserId(userJson[0].id);
+            user.setBalance(userJson[0].balance);
+
+            resolve(user);
+           });
+        });
+
+    }
+
+
+    updateBalance(user, valuePayment){
+        let newValue;
+        if(user['balance'] == null){
+            newValue = 0 + valuePayment;
+        }else{
+            newValue = user['balance'] + valuePayment;
+        }
+            db.query('UPDATE Users SET balance = ? WHERE id = ?', [newValue, user['id']], (err, res) =>{
+                if(err) throw err;
+                console.log(res['serverStatus'])
+            });
+
+    }
+
+}
+
+
+module.exports = new UserController();
